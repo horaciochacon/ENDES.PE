d121718107abf5f68246288da0c43f77baad99e6
horaciochacon <42793704+horaciochacon@users.noreply.github.com>
2018-09-28 21:09:02 -0500

    Agregado opción de guardar

--- R/consulta_endes.R
+++ R/consulta_endes.R
@@ -10,20 +10,33 @@
 #'
 #' @export consulta_endes
 
-consulta_endes <- function(periodo, codigo_modulo, base) {
-  versiones <- matrix(c(2017,605,2016,548,2015,504,2014,441,2013,
-                        407,2012,323,2011,290,2010,260),byrow = T,ncol = 2)
+consulta_endes <- function(periodo, codigo_modulo, base, guardar = TRUE, ruta) {
+  # Generamos dos objetos temporales: un archivo y una carpeta 
+  temp <- tempfile() ; tempdir <- tempdir()
+  
+  # Genera una matrix con el número identificador de versiones por cada año
+  versiones <- matrix(c(2017,605,2016,548,2015,504,2014,441,2013,407,2012,323,2011,290,2010,260),byrow = T,ncol = 2)
+  
+  # Extrae el código de la encuesta con la matriz versiones
   codigo_encuesta <- versiones[versiones[,1] == periodo,2]
-  ruta_base <- 'http://iinei.inei.gob.pe/iinei/srienaho/descarga/SPSS/'
-  modulo <- paste('-modulo',codigo_modulo,'.zip',sep = '')
-  filename <- paste(ruta_base,codigo_encuesta,modulo,sep = '')
-  temp <- tempfile()
-  download.file(filename,temp)
+  ruta_base <- "http://iinei.inei.gob.pe/iinei/srienaho/descarga/SPSS/" # La ruta de microdatos INEI
+  modulo <- paste("-modulo",codigo_modulo,".zip",sep = "")
+  url <- paste(ruta_base,codigo_encuesta,modulo,sep = "")
+  
+  # Descargamos el archivo
+  download.file(url,temp)
+  
+  # Listamos los archivos descargados y seleccionamos la base elegida
   archivos <- unzip(temp,list = T)
   archivos <- archivos[str_detect(archivos$Name, base) == TRUE,]
-  DF <-unzip(temp,files = archivos$Name)
-  read.spss(DF,to.data.frame = T)
+  
+  
+  # Elegimos entre guardar los archivos o pasarlos directamente a un objeto
+  if(guardar == TRUE) {
+    unzip(temp,files = archivos$Name,exdir = paste(getwd(),ruta,sep = "/"))
+  } else {
+    read.spss(unzip(temp, files = archivos$Name, exdir = tempdir),to.data.frame = T)
+  }
 }
-
 
 
