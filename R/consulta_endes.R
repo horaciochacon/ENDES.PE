3016b465e92ac07e66ab8778a44cfdad561fb43d
horaciochacon <horacio.chacon.t@upch.pe>
2019-04-14 23:45:43 -0500

    Actualización de Versiones

--- R/consulta_endes.R
+++ R/consulta_endes.R
@@ -15,7 +15,10 @@ consulta_endes <- function(periodo, codigo_modulo, base, guardar = FALSE, ruta =
   temp <- tempfile() ; tempdir <- tempdir()
   
   # Genera una matriz con el número identificador de versiones por cada año
-  versiones <- matrix(c(2017,605,2016,548,2015,504,2014,441,2013,407,2012,323,2011,290,2010,260),byrow = T,ncol = 2)
+  versiones <- matrix(c(2017,605,2016,548,2015,504,2014,441,
+                        2013,407,2012,323,2011,290,2010,260,
+                        2009,238,2008,209,2007,194,2006,183,
+                        2005,150,2004,120),byrow = T,ncol = 2)
   
   # Extrae el código de la encuesta con la matriz versiones
   codigo_encuesta <- versiones[versiones[,1] == periodo,2]
