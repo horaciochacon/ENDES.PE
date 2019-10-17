306f8d007ecdf458910904b900be9b3cf01d48b7
Horacio Chacón <horaciochacon89@gmail.com>
2019-10-17 18:58:26 -0500

    Añadidos ejemplos y descripción de parámetros

--- R/diccionario_encuesta.R
+++ R/diccionario_encuesta.R
@@ -2,28 +2,31 @@
 #'
 #' @description Funcion para generar diccionario de variables con las bases de datos de la ENDES o ENAHO.
 #'
-#' @param base_endes
+#' @param base_endes Base Endes o Enaho
 #'
 #' @return tibble
 #'
 #' @examples 
 #' 
-#' endes <- consulta_endes(periodo = 2012,codigo_modulo = '64',base = 'RECH1', guardar = F)
+#' endes <- consulta_endes(periodo = 2012,codigo_modulo = '64',
+#' base = 'RECH1', guardar = FALSE, codificacion = "UTF-8")
 #' diccionario_encuesta(base_endes=endes)
 #' 
-#' enaho <- consulta_enaho(periodo = 2017, codigo_modulo = "01", base = "Enaho01-2017-100", guardar = FALSE)
+#' enaho <- consulta_enaho(periodo = 2017, codigo_modulo = "01", 
+#' base = "Enaho01-2017-100", guardar = FALSE, codificacion = "latin1")
+#' 
 #' diccionario_encuesta(base_endes=enaho)
 #'
+#'@importFrom magrittr %>%
+#'@importFrom rlang .data
+#'
 #' @export diccionario_encuesta
+#' 
 
 diccionario_encuesta <- function(base_endes) {
   base_endes %>% 
-    as_factor() %>% 
-    look_for(details = T) %>% 
-    mutate(
-      #retirar luego de corregir utf-8
-      #label=make_clean_names(label)
-    ) %>% 
-    as_tibble() %>% 
-    select(variable,class,label,levels) %>% print(n=Inf)
+    sjlabelled::as_factor() %>% 
+    labelled::look_for(details = TRUE) %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::select(.data$variable,class,.data$label,levels) %>% print(n=Inf)
 }
--- R/unir_endes.R
+++ R/unir_endes.R
@@ -2,11 +2,24 @@
 #'
 #' @description Funcion para unir bases de datos de la ENDES
 #'
-#' @param base1,base2,tipo_union
+#' @param base1 Base Endes o Enaho
+#' @param base2 Base Endes o Enaho
+#' @param tipo_union Tipo de unión (Hogar o Individual)
 #'
 #' @return Data.Frame
 #'
 #' @examples 
+#' 
+#' persona   <- consulta_endes(periodo = 2017, codigo_modulo = 64, 
+#' base = "RECH1", guardar = FALSE, codificacion = "latin1")
+#' 
+#' salud     <- consulta_endes(periodo = 2017, codigo_modulo = 414, 
+#' base = "CSALUD01", guardar = FALSE, codificacion = "latin1")
+#' 
+#' unir_endes(base1 = persona, base2 = salud, tipo_union = "individual")
+#' 
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
 #'
 #' @export unir_endes
 
@@ -14,45 +27,48 @@ unir_endes <- function(base1, base2, tipo_union = "individual") {
   
   if("CASEID" %in% names(base1)) {
     base1 <-  base1 %>% 
-      mutate(HHID = str_sub(CASEID,1,(str_length(CASEID)-3)),
-             QSNUMERO = as.numeric(str_sub(CASEID,-2,-1)))
+      dplyr::mutate(HHID = stringr::str_sub(.data$CASEID,1,
+                                            (stringr::str_length(.data$CASEID)-3)),
+             QSNUMERO = as.numeric(stringr::str_sub(.data$CASEID,-2,-1)))
   }
   
   if("CASEID" %in% names(base2)) {
     base2 <-  base2 %>% 
-      mutate(HHID = str_sub(CASEID,1,(str_length(CASEID)-3)),
-             QSNUMERO = as.numeric(str_sub(CASEID,-2,-1)))
+      dplyr::mutate(
+        HHID = stringr::str_sub(.data$CASEID,1,
+                                (stringr::str_length(.data$CASEID)-3)),
+             QSNUMERO = as.numeric(stringr::str_sub(.data$CASEID,-2,-1)))
   }
   
   if("HVIDX"  %in% names(base1)) {
-    base1 <- rename(base1, QSNUMERO = HVIDX)
+    base1 <- dplyr::rename(base1, QSNUMERO = .data$HVIDX)
   }
   
   if("HVIDX"  %in% names(base2)) {
-    base2 <- rename(base2, QSNUMERO = HVIDX)
+    base2 <- dplyr::rename(base2, QSNUMERO = .data$HVIDX)
   }
   if("HC0"  %in% names(base1)) {
-    base1 <- rename(base1, QSNUMERO = HC0)
+    base1 <- dplyr::rename(base1, QSNUMERO = .data$HC0)
   }
   
   if("HC0"  %in% names(base2)) {
-    base2 <- rename(base2, QSNUMERO = HC0)
+    base2 <- dplyr::rename(base2, QSNUMERO = .data$HC0)
   }
   
   if("HA0"  %in% names(base1)) {
-    base1 <- rename(base1, QSNUMERO = HA0)
+    base1 <- dplyr::rename(base1, QSNUMERO = .data$HA0)
   }
   
   if("HA0"  %in% names(base2)) {
-    base2 <- rename(base2, QSNUMERO = HA0)
+    base2 <- dplyr::rename(base2, QSNUMERO = .data$HA0)
   }
   
   if(tipo_union == "individual")  {
-    left_join(base1,base2, by = c("HHID","QSNUMERO"))
+    dplyr::left_join(base1,base2, by = c("HHID","QSNUMERO"))
   } 
   
   else if(tipo_union == "hogar") {
-    left_join(base1,base2, by = "HHID")
+    dplyr::left_join(base1,base2, by = "HHID")
   }
   
   else {
